467a233a90888859a8e8b1b470b43ef773cbdfb1
TheKCui0327 <59521762+KCui0327@users.noreply.github.com>
2022-07-18 17:51:51 -0400

    main

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include "input.c"
+#include "textDisplay.c"
 
+int main(int argc, char* argv[]) {
+    printf("%s", introString);
+    printf("%s", functionString);
+
+    
+    return 0;
+}
