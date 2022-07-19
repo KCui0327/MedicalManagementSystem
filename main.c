bcc7d9f897bb05915d82e7696f92ad5945d1c466
TheKCui0327 <59521762+KCui0327@users.noreply.github.com>
2022-07-18 21:42:22 -0400

    main

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include "getInput.c"
 #include "textDisplay.c"
@@ -12,7 +13,7 @@ int main(int argc, char* argv[]) {
     printf("%s", introString);
     printf("%s\n\n", functionString);
 
-    
+
 
     do {
         printf("Enter command: ");
