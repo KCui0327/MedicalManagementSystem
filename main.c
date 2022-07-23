2c96d7108e4245be61c804a208a260b9f61cc3d5
TheKCui0327 <59521762+KCui0327@users.noreply.github.com>
2022-07-23 03:49:34 -0400

    removed unnecessary files

--- main.c
+++ main.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "getInput.c"
-#include "textDisplay.c"
-#include "nodeOperations.c"
+#include <stdbool.h>
 #define MAX_SIZE 1000
+
+typedef struct node {
+    char *familyName;
+    char *firstName;
+    char *address;
+    char *phoneNumber;
+    char *status;
+    char *condition;
+    struct node *next;
+} Node;
+
+typedef struct linkedList {
+    Node *head;
+} LinkedList;
+
+void getCommand(char *input, int inputSize);
+void getInputNode(char *familyName, char *firstName, char *address, char *phoneNumber, char *doctorName, char *status, char *condition);
+Node* createNode(char *familyName, char *firstName, char *address, char *phoneNumber, char *status, char *condition);
+void printNode(LinkedList *list);
+bool isEmpty(LinkedList *list);
+void addNodeFront(LinkedList *list, char *familyName, char *firstName, char *address, char *phoneNumber, char *status, char *condition);
+
 
 int main(int argc, char* argv[]) {
     char input[MAX_SIZE + 1], command;
     char *familyName, *firstName, *address, *phoneNumber, *doctorName, *status, *condition;
+    const char introString[] 
+    = "Medical Management System.\n\n";
+const char functionString[] 
+    = "List of Commands: \n\t - Insert (I) \n\t - Delete (D) \n\t - S (search by name) \n\t - P (search by phone number) \n\t - Print (P) \n\t - Reset (R) \n\t - Exit (E)";
 
     LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
     list->head = NULL;
@@ -48,4 +72,72 @@ int main(int argc, char* argv[]) {
     } while (command != 'E');
 
     return 0;
+}
+
+void getCommand(char *input, int inputSize) {
+    int i;
+    char c;
+    for (i = 0; (c = getchar()) != '\n' && i < (inputSize - 1); i++) {
+        input[i] = c;
+    }
+    input[i] = '\0';
+}
+
+void getInputNode(char *familyName, char *firstName, char *address, char *phoneNumber, char *doctorName, char *status, char *condition) {
+    printf("\n  Family name: ");
+    getCommand(familyName, MAX_SIZE);
+    printf("  First name: ");
+    getCommand(firstName, MAX_SIZE);
+    printf("  Address: ");
+    getCommand(address, MAX_SIZE);
+    printf("  Phone number: ");
+    getCommand(phoneNumber, MAX_SIZE);
+    printf("  Status: ");
+    getCommand(status, MAX_SIZE);
+    printf("  Condition: ");
+    getCommand(condition, MAX_SIZE);
+}
+
+Node* createNode(char *familyName, char *firstName, char *address, char *phoneNumber, char *status, char *condition) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode != NULL) {
+        newNode->familyName = familyName;
+        newNode->firstName = firstName;
+        newNode->address = address;
+        newNode->phoneNumber = phoneNumber;
+        newNode->status = status;
+        newNode->condition = condition;
+        newNode->next = NULL;
+    }
+    return newNode;
+}
+
+void printNode(LinkedList *list) {
+    Node *currentNode = list->head;
+    while (currentNode != NULL) {
+        printf("\n  Family name: %s", currentNode->familyName);
+        printf("\n  First name: %s", currentNode->firstName);
+        printf("\n  Address: %s", currentNode->address);
+        printf("\n  Phone number: %s", currentNode->phoneNumber);
+        printf("\n  Status: %s", currentNode->status);
+        printf("\n  Condition: %s", currentNode->condition);
+        currentNode = currentNode->next;
+    }
+}
+
+bool isEmpty(LinkedList *list) {
+    return list->head == NULL;
+}
+
+void addNodeFront(LinkedList *list, char *familyName, char *firstName, char *address, char *phoneNumber, char *status, char *condition) {
+    if (isEmpty(list)) {
+        list->head = createNode(familyName, firstName, address, phoneNumber, status, condition);
+        return;
+    } 
+
+    Node *temp = createNode(familyName, firstName, address, phoneNumber, status, condition);
+    if (temp != NULL) {
+        temp->next = list->head;
+        list->head = temp;
+    }
 }
--- textDisplay.c
+++
@@ -1,4 +0,0 @@
-const char introString[] 
-    = "Medical Management System.\n\n";
-const char functionString[] 
-    = "List of Commands: \n\t - Insert (I) \n\t - Delete (D) \n\t - S (search by name) \n\t - P (search by phone number) \n\t - Print (P) \n\t - Reset (R) \n\t - Exit (E)";
